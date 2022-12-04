dbc28b81e0fa6651273ce6ef98cd822353601503
shizatallat26 <shizatallat@gmail.com>
2022-12-04 17:36:30 +0500

    Assignment-2

---
+++ app.js
@@ -0,0 +1,236 @@
+// function btnClick(input){
+//     alert(input);
+// }
+
+
+// function test(){
+//     var test = document.getElementById("hello");
+
+//     test.style.color = 'blue';
+// }
+
+
+// function textFocus(){
+//     var text = document.getElementById("firstName");
+
+//     text.style.backgroundColor = "red";
+// }
+
+
+// function textBlur(){
+//     var text = document.getElementById("firstName");
+
+//     text.style.backgroundColor = "green";
+// }
+
+
+
+// function demo(){
+//     var fn = document.getElementById("fn");
+//     var ln = document.getElementById("ln");
+
+//     fn.style.backgroundColor = "red";
+//     ln.style.fontSize = "40px";
+//     ln.style.fontFamily = "comic sans ms";
+// }
+
+
+// function rightClick(input){
+//     alert(input);
+// }
+
+
+// function doubleClick(input){
+//     alert(input);
+// }
+
+
+// function timeChange(){
+//     alert("This is on duration change text");
+// }
+
+
+// function playAud() {
+//     alert("Can start playing video");
+// }
+
+
+// function playThrough() {
+//     alert("Can play through video without stopping");
+//   }
+
+
+// function copyTxt(){
+//     document.getElementById("demo").innerHTML = "You copied this text";
+// }
+
+
+// function cutTxt(){
+//     document.getElementById("demo").innerHTML = "You cut this text";
+// }
+
+
+// function myFunction() {
+//     alert("The video has ended");
+//   }
+
+
+// function errorImg() {
+//     alert('The image could not be loaded.');
+// }
+
+
+
+// function focusTxt(focus) {
+//     focus.style.background = "yellow";
+// }
+
+
+// function myFunction() {
+//     let fName = document.getElementById("fname");
+//     fName.value = fName.value.toUpperCase();
+//     fName.style.backgroundColor = "pink";
+//   }
+
+
+// function myFunction(){
+//     var input = document.getElementById("myInput").value;
+
+//     document.getElementById("demo").innerHTML = "You Write: " + input;
+
+// }
+
+
+// function myFunction(){
+//     alert("You pressed a key inside the input field");
+// }
+
+
+// function myFunction(){
+//     alert("You pressed a key inside the input field");
+// }
+
+
+// function myFunction(){
+//     var input = document.getElementById("keyUp");
+//     input.value = input.value.toUpperCase();
+//     input.style.backgroundColor = "yellow";
+// }
+
+
+// function myFunction(){
+//     alert("Page is loaded");
+// }
+
+
+// function myFunction() {
+//     alert("Browser has loaded the current frame");
+// }
+
+
+
+// function myFunction() {
+//   alert("Meta data for video loaded");
+// }
+
+
+// function mouseDown(){
+//     var click = document.getElementById("myPara");
+//     click.style.color = "red";
+// }
+
+// function mouseUp(){
+//     var click = document.getElementById("myPara");
+//     click.style.color = "green";
+// }
+
+
+// function bigImg(gif){
+//     gif.style.height = "64px";
+//     gif.style.width = "64px";
+// }
+  
+// function normalImg(gif){
+//     gif.style.height = "32px";
+//     gif.style.width = "32px";
+// }
+
+
+// function myFunction(){
+//     alert("The video was paused.");
+// }
+
+
+
+// function myFunction(){
+//     alert("The video is now playing");
+// }
+
+
+// function myFunction(){
+//     alert("Downloading video");
+// }
+
+
+// function myFunction(){
+//     alert("The form was reset");
+// }
+
+
+// function myFunction() {
+//     var w = window.outerWidth;
+//     var h = window.outerHeight;
+//     var txt = "Window size: width=" + w + ", height=" + h;
+//     document.getElementById("demo").innerHTML = txt;
+//   }
+
+
+// function myFunction(){
+//     var width = window.outerWidth;
+//     var height = window.outerHeight;
+//     var size = "Windoe size: widhth = " + width + ", height = " + height;
+//     document.getElementById("demo").innerHTML = size;
+// }
+
+
+// let x = 0;
+// function myFunction() {
+//   document.getElementById("demo").innerHTML = x += 1;
+// }
+
+
+// function myFunction() {
+//     alert("Seek operation completed!");
+// }
+
+
+// function myFunction() {
+//     alert("Seek operation began!");
+// }
+
+
+// function myFunction() {
+//     alert("You selected some text!");
+// }
+
+
+
+// function myFunction() {
+//     alert("The form was submitted");
+// }
+
+
+// function myFunction(event) {
+//     // The currentTime property returns the current position of the audio/video playback
+//     document.getElementById("demo").innerHTML = event.currentTime;
+// }
+
+
+// function myFunction() {
+//     alert("The volume has been changed!");
+
+
+
+function myFunction() {
+    alert("Wait! I need to buffer the next frame");
+}
